a089fcba42dc8ba6d0f3ad781f2483c4c51dcb5d
Südi Tamás <sudi.tamas@simonyi.bme.hu>
2022-04-13 14:52:14 +0200

    Create primsa schema

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MyLogger } from './logger';
+// import { MyLogger } from './logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: new MyLogger(),
+    //  logger: new MyLogger(),
   });
 
   await app.listen(3000);
